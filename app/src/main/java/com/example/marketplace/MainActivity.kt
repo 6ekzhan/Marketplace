fd5685a2315cf1e8fb12e852698e187c7ec58a32
6ekzhan <201439@astanait.edu.kz>
2022-04-24 21:04:30 +0600

    login and register

--- app/src/main/java/com/example/marketplace/MainActivity.kt
+++ app/src/main/java/com/example/marketplace/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.marketplace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userId = intent.getStringExtra("user_id")
+        val emailId = intent.getStringExtra("email_id")
+        tv_user_id.setText("User ID:: $userId")
+        tv_email_id.setText( "Email ID:: $emailId")
+
+        button.setOnClickListener{
+            //Logout
+            FirebaseAuth.getInstance().signOut()
+
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/marketplace/Register.kt
@@ -0,0 +1,77 @@
+package com.example.marketplace
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_register.*
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        tv_register.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
+
+        button.setOnClickListener {
+            when {
+                TextUtils.isEmpty(email_et.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this,
+                        "Please enter email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(
+                    password_et.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this,
+                        "Please enter password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    val email: String = email_et.text.toString().trim { it <= ' ' }
+                    val password: String = password_et.text.toString().trim { it <= ' ' }
+
+                    //Create an instance and create a register a user with email and password.
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            //if registration is done
+                            if (task.isSuccessful) {
+                                //Firebase register user
+                                val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                Toast.makeText(
+                                    this,
+                                    "You were registered successfully.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val intent = Intent(this, MainActivity::class.java)
+                                intent.flags =
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_id", firebaseUser.uid)
+                                intent.putExtra("email_id", email)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                // if registration is failed
+                                Toast.makeText(
+                                    this,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                }
+            }
+        }
+    }
+}
