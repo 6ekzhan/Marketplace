5afb0ba3dbe91dc9f793693fd62d8da3bc0a5bd1
6ekzhan <201439@astanait.edu.kz>
2022-05-03 01:34:58 +0600

    login and main pages

--- app/src/main/java/com/example/marketplace/MainActivity.kt
+++ app/src/main/java/com/example/marketplace/MainActivity.kt
@@ -1,27 +1,24 @@
 package com.example.marketplace
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.FirebaseAuth
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var registerButton: Button? = null
+    private var loginButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userId = intent.getStringExtra("user_id")
-        val emailId = intent.getStringExtra("email_id")
-        user_id.setText("User ID:: $userId")
-        email_id.setText( "Email ID:: $emailId")
+        registerButton = findViewById(R.id.register_btn)
+        loginButton = findViewById(R.id.login_btn)
 
-        button.setOnClickListener{
-            //Logout
-            FirebaseAuth.getInstance().signOut()
-
+        login_btn.setOnClickListener {
             startActivity(Intent(this, Login::class.java))
-            finish()
         }
     }
 }
